f86f70ca0ef75143634211812cee8555ffae82a5
Melora-Mendonca <melora.mendonca@city.ac.uk>
2026-03-06 19:36:16 +0000

    Added Login Form

--- src/IPOS/SA/ACC/LoginForm.java
+++ src/IPOS/SA/ACC/LoginForm.java
@@ -1,10 +1,27 @@
 package IPOS.SA.ACC;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginForm {
+public class LoginForm extends JFrame{
     private JPanel MainPanel;
     private JPanel loginPanel;
 
+    public LoginForm() {
+    }
+
+
+    public static void main(String[] a){
+        //Creating object of LoginFrame class and setting some of its properties
+        LoginForm frame = new LoginForm();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10,10,370,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+    }
 
 }
